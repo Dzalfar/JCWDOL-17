a00c6e41bbf074b8ab2f497a36d55cf14ff1598e
Dzalfar <hafidzalghi@gmail.com>
2024-11-13 18:53:12 +0700

    Create object.js
    commit to main
    
    Co-Authored-By: Jordan <74591126+jordansmrd@users.noreply.github.com>

---
+++ fundamental/day 6/object.js
@@ -0,0 +1,14 @@
+const car1 = {
+    brand: "BMW",
+    model: "M5",
+    price: 800000,
+  };
+  
+  const car2 = {
+    brand: "Toyota",
+    model: "Corolla",
+    price: 7000000,
+  };
+  
+  console.log(car1, car2);
+  
